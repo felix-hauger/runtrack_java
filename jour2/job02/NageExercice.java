8863d1cfadf4d49e0697bdb590b3f78e774e00de
felix-hauger <felix.hauger@laplateforme.io>
2023-09-07 15:18:47 +0200

    Add NageExercice file in jour2 job02

---
+++ jour2/job02/NageExercice.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jour2.job02;
+
+/**
+ *
+ * @author fel
+ */
+public class NageExercice {
+    
+    public static void main(String[] args) {
+        Homme homme = new Homme();
+
+        homme.nager();
+        
+        Poisson poisson = new Poisson();
+        
+        poisson.nager();
+
+    }
+}
